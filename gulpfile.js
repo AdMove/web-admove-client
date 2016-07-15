76eee4d3ccfb8376ef4dff016a6848192c3858fc
Giorgi <giorgirokhadze@gmail.com>
2016-07-15 11:57:53 +0400

    Minor reorganisation of code structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var gulp = require('gulp');
 
 // plugins
 var connect = require('gulp-connect');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+	return gulp.src('app/**/*')
+		.pipe(ghPages());
+});
 
 gulp.task('connect', function () {
 	connect.server({
